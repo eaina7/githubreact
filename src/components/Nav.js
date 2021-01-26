25eaafb829640ec982dd0e520d45a58516498098
eaina7 <eaina7gmail.com>
2021-01-26 17:49:51 +0100

    app and nav created

---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Nav() {
+    return (
+        <div>
+            <Nav>
+                <h3>Eric</h3>
+                <h3>Stefan</h3>
+                <h3>Waldemar</h3>
+            </Nav>
+        </div>
+    )
+}
+
+export default Nav
