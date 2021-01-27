50908621570fec79ff10360efd0036bf01a06002
waldemar-bl <waldemar.blex@googlemail.com>
2021-01-27 20:47:35 +0100

    use value from input-field

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import { useState } from "react"; 
+import { useState } from "react";
 import { Switch, Route, useHistory } from "react-router-dom";
-import NavBar from './components/Nav/';
-import DetailedProfile from './components/view/detailedProfile';
-import rootPage from './components/view/rootPage';
-import './App.css';
-import './theme.css';
- 
-function App() { 
-	 const [theme, setTheme] = useState(true); 
-	 const toggleTheme = () => { 
-	 	 setTheme(!theme)
-	 }  
+import NavBar from "./components/Nav/";
+import DetailedProfile from "./components/view/detailedProfile";
+import rootPage from "./components/view/rootPage";
+import "./App.css";
+import "./theme.css";
+import Form from "./components/Form";
+
+function App() {
+  const [theme, setTheme] = useState(true);
+  const toggleTheme = () => {
+    setTheme(!theme);
+  };
   return (
-    <>  
-	    <div  className={`${theme ? "grey" : "white"} App theme`}> 
-		    <NavBar toggle={toggleTheme} />
-		    <Switch>
-		      <Route path="/details/:detailedProfileName" component={DetailedProfile} /> 
-		      <Route path="/" component={rootPage} />  
-		    </Switch>  
-	    </div>
+    <>
+      <div className={`${theme ? "grey" : "white"} App theme`}>
+        <NavBar toggle={toggleTheme} />
+        <Form />
+        <Switch>
+          <Route
+            path="/details/:detailedProfileName"
+            component={DetailedProfile}
+          />
+          <Route path="/" component={rootPage} />
+        </Switch>
+      </div>
     </>
   );
-} 
-export default App;   
+}
+export default App;
